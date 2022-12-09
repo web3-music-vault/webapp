431cfedb2b021f37ea78352516890468b5f4f971
PrizemArtNFT <116934352+PrizemArtNFT@users.noreply.github.com>
2022-12-08 21:23:23 -0800

    adding ampllify backend

--- next.config.js
+++ next.config.js
@@ -2,14 +2,5 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'gateway.ipfscdn.io',
-        pathname: '/ipfs/**',
-      },
-    ],
-  },
 }
 module.exports = nextConfig
