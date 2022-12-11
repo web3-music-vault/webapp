39da719249de25a91dddc1c37c346edcbf87d968
PrizemArtNFT <116934352+PrizemArtNFT@users.noreply.github.com>
2022-12-11 12:02:52 -0800

    changing name to web3 music vault in signup message

--- lib/createMessage.ts
+++ lib/createMessage.ts
@@ -1,5 +1,5 @@
 export function createMessage(walletId:string, nonce:string){
-    return `Login to Smart Media Vault using wallet
+    return `Login to Web3 Music Vault using wallet
     address: ${walletId?.toString()}
     accessKey: ${nonce}`
 }
